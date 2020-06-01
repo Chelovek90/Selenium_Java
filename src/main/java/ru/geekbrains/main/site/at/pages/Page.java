c8d9b847de0563dbee625f4a9d427833ef791115
Alexandr Shadskiy <homosapiens-90@mail.ru>
2020-06-01 15:02:48 +0300

    In work

--- src/main/java/ru/geekbrains/main/site/at/pages/Page.java
+++ src/main/java/ru/geekbrains/main/site/at/pages/Page.java
@@ -11,11 +11,28 @@ public class Page extends BasePage {
     @FindBy(className = "gb-header__title")
     private WebElement pageHeader;
 
+    @FindBy(css = "#top-menu .show-search-form")
+    private WebElement searchButton;
+
+    @FindBy(css = "#top-menu .search-panel__search-field")
+    private WebElement searchInput;
+
+    @FindBy(css = "#top-menu .show-search-form")
+    private WebElement searchOpenButton;
+
+    @FindBy(css = "#top-menu .search-panel__search-field")
+    private WebElement searchFieldInput;
+
+    @FindBy(className = "search-panel__submit")
+    private WebElement searchStart;
+
     private NavigationTab navigationTab;
+    private SearchPageTab searchPageTab;
 
     public Page(WebDriver driver) {
         super(driver);
         navigationTab = new NavigationTab(driver);
+        searchPageTab = new SearchPageTab(driver);
     }
 
     public Page checkHeader(String expected) {
@@ -23,7 +40,18 @@ public class Page extends BasePage {
         return this;
     }
 
+    public Page searchValue (String value) {
+        searchOpenButton.click();
+        searchFieldInput.sendKeys(value);
+        searchStart.click();
+        return this;
+    }
+
     public NavigationTab getNavigationTab() {
         return navigationTab;
     }
+    public SearchPageTab getSearchPageTab() {
+        return searchPageTab;
+    }
+
 }
--- src/test/java/ru/geekbrains/main/site/at/SearchTest.java
+++ src/test/java/ru/geekbrains/main/site/at/SearchTest.java
@@ -1,55 +1,39 @@
-//package ru.geekbrains.main.site.at;
-//
-//import org.junit.jupiter.api.Test;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.*;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//public class SearchTest extends Locators {
-//    @Test
-//    public void searchTest() {
-//        /*Открываю стартовую страницу теста*/
-//        open(BASE_URL + "/courses");
-//        driver.manage().window().maximize();
-//        /*Закрываю всплывающее окно скидки*/
-//        closeSell();
-//        /*Открываю поисковую строку*/
-//        driver.findElement(searchButton).click();
-//        /*Ввожу в строку поиска слово*/
-//        driver.findElement(searchInput).sendKeys("java");
-//
-//        /*Проверяю, что появилась панель с результатами поиска*/
-//        WebDriverWait wait = new WebDriverWait(driver, 15);
-//        wait.until(ExpectedConditions.presenceOfElementLocated(searchTab));
-//
-//        /*Выполняю проверку результатов найденых элементов по категориям*/
-//        result = Integer.parseInt(getText(professionsHeaderButton));
-////        assertTrue(result >= 2, "Результат поиска по категории 'Профессии' не соответствует");
-//        assertThat(result, lessThanOrEqualTo(2));
-//
-//        result = Integer.parseInt(getText(courseHeaderButton));
-////        assertTrue(result > 15, "Результат поиска по категории 'Курсы' не соответствует");
-//        assertThat(result, greaterThan(15));
-//
-//        result = Integer.parseInt(getText(vebinarHeaderButton));
-////        assertTrue(180 < result && result < 300, "Результат поиска по категории 'Вебинары' не соответствует");
-//        assertThat(result, allOf(
-//               greaterThan(180),
-//               lessThan(300)
-//        ));
-//        result = Integer.parseInt(getText(blogHeaderButton));
-////        assertTrue(result > 300, "Результат поиска по категории 'Блог' не соответствует");
-//        assertThat(result, greaterThan(300));
-//
-//        result = Integer.parseInt(getText(forumHeaderButton));
-////        assertTrue(result != 350, "Результат поиска по категории 'Форумы' не соответствует");
-//        assertThat(result, not(equalTo(350)));
-//
-//        result = Integer.parseInt(getText(testHeaderButton));
-////        assertTrue(result != 0, "Результат поиска по категории 'Тесты' не соответствует");
-//        assertThat(result, not(equalTo(0)));
-//    }
-//}
+package ru.geekbrains.main.site.at;
+
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.By;
+import ru.geekbrains.main.site.at.Base.BaseTest;
+import ru.geekbrains.main.site.at.pages.Page;
+
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+public class SearchTest extends BaseTest {
+
+    @BeforeEach
+    public void openPage() {
+        driver.get(BASE_URL + "/courses");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//div/div/button[2]")).click();
+    }
+
+    @ParameterizedTest
+    @MethodSource("selectorGenerator")
+    public void searchFieldTest(String value, String selector) {
+        new Page(driver)
+                .searchValue(value)
+                .getSearchPageTab()
+                .shouldBeCorrectResult(selector);
+    }
+
+    public static Stream<String> selectorGenerator() {
+        return Stream.of("Профессии", "Курсы", "Вебинары", "Блоги", "Форумы", "Тесты");
+    }
+}
